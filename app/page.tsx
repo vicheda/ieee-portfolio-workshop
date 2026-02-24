44cf8f9f209cd9714730e86f1e88478906c81f41
vichedanarith <vichedanarith2027@u.northwestern.edu>
2026-02-23 19:09:12 -0600

    fix a bug

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import AboutMe from "./components/Aboutme";
+import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
